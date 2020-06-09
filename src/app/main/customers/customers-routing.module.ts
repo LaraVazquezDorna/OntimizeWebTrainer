2a375e156b27c09578452aa0588e58361e386257
Lara Vázquez Dorna <laritavdorna@gmail.com>
2020-06-09 12:43:09 +0200

    Detalle Insertar

--- src/app/main/customers/customers-routing.module.ts
+++ src/app/main/customers/customers-routing.module.ts
@@ -2,15 +2,18 @@ import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {CustomersHomeComponent} from "./customers-home/customers-home.component";
 import {CustomersDetailComponent} from "./customers-detail/customers-detail.component";
+import {CustomersNewComponent} from "./customers-new/customers-new.component";
 
 const routes: Routes = [{
+    path: "new",
+    component: CustomersNewComponent
+}, {
+    path: ":CUSTOMERID",
+    component: CustomersDetailComponent
+}, {
     path: '',
     component: CustomersHomeComponent
-},
-    {
-        path: ":CUSTOMERID",
-        component: CustomersDetailComponent
-    }
+}
 ];
 
 @NgModule({
