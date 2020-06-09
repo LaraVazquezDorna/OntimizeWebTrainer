2e34dc7525704f7c568e664e490a1bc3b1f2a8d9
Lara Vázquez Dorna <laritavdorna@gmail.com>
2020-06-09 14:08:55 +0200

    Renderer columna

--- src/app/main/accounts/accounts.module.ts
+++ src/app/main/accounts/accounts.module.ts
@@ -4,6 +4,7 @@ import {CommonModule} from '@angular/common';
 import {AccountsRoutingModule} from './accounts-routing.module';
 import {OntimizeWebModule} from "ontimize-web-ngx";
 import {AccountsHomeComponent} from "./accounts-home/accounts-home.component";
+import {AccountNumberRenderComponent} from "./accounts-home/account-number-render/account-number-render.component";
 
 @NgModule({
     imports: [
@@ -12,7 +13,8 @@ import {AccountsHomeComponent} from "./accounts-home/accounts-home.component";
         AccountsRoutingModule
     ],
     declarations: [
-        AccountsHomeComponent
+        AccountsHomeComponent,
+        AccountNumberRenderComponent
     ]
 })
 export class AccountsModule {
