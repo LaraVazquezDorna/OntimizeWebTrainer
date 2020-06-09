352f4de3a301e5c1d3b0aee86d11a4f45f6e93e0
Lara Vázquez Dorna <laritavdorna@gmail.com>
2020-06-09 12:36:05 +0200

    DETALLE Mostrar, editar y borrar

--- src/app/main/customers/customers-routing.module.ts
+++ src/app/main/customers/customers-routing.module.ts
@@ -1,14 +1,21 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 import {CustomersHomeComponent} from "./customers-home/customers-home.component";
+import {CustomersDetailComponent} from "./customers-detail/customers-detail.component";
 
 const routes: Routes = [{
-  path : '',
-  component: CustomersHomeComponent
-}];
+    path: '',
+    component: CustomersHomeComponent
+},
+    {
+        path: ":CUSTOMERID",
+        component: CustomersDetailComponent
+    }
+];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
 })
-export class CustomersRoutingModule { }
+export class CustomersRoutingModule {
+}
--- src/app/main/customers/customers.module.ts
+++ src/app/main/customers/customers.module.ts
@@ -1,17 +1,20 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { OntimizeWebModule } from "ontimize-web-ngx";
-import { CustomersRoutingModule } from './customers-routing.module';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {OntimizeWebModule} from "ontimize-web-ngx";
+import {CustomersRoutingModule} from './customers-routing.module';
 import {CustomersHomeComponent} from "./customers-home/customers-home.component";
+import {CustomersDetailComponent} from "./customers-detail/customers-detail.component";
 
 @NgModule({
-  imports: [
-    CommonModule,
-    OntimizeWebModule,
-    CustomersRoutingModule
-  ],
-  declarations: [
-    CustomersHomeComponent
-  ]
+    imports: [
+        CommonModule,
+        OntimizeWebModule,
+        CustomersRoutingModule
+    ],
+    declarations: [
+        CustomersHomeComponent,
+        CustomersDetailComponent
+    ]
 })
-export class CustomersModule { }
+export class CustomersModule {
+}
