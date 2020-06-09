98f027c9957655a4e6cf395723477662c9ef0c36
Lara Vázquez Dorna <laritavdorna@gmail.com>
2020-06-09 13:47:19 +0200

    Sucursales

--- src/app/main/main-routing.module.ts
+++ src/app/main/main-routing.module.ts
@@ -1,11 +1,12 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { AuthGuardService } from 'ontimize-web-ngx';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {AuthGuardService} from 'ontimize-web-ngx';
 
-import { MainComponent } from './main.component';
-import { HomeModule } from './home/home.module';
-import { CustomersModule } from './customers/customers.module';
-import { EmployeesModule } from './employees/employees.module';
+import {MainComponent} from './main.component';
+import {HomeModule} from './home/home.module';
+import {CustomersModule} from './customers/customers.module';
+import {EmployeesModule} from './employees/employees.module';
+import {BranchesModule} from "./branches/branches.module";
 
 export function loadHomeModule() {
     return HomeModule;
@@ -15,8 +16,12 @@ export function loadCustomersModule() {
     return CustomersModule;
 }
 
-export function loadEmployeesModule(){
+export function loadEmployeesModule() {
     return EmployeesModule;
+}
+
+export function loadBranchesModule() {
+    return BranchesModule;
 }
 
 export const routes: Routes = [
@@ -25,10 +30,11 @@ export const routes: Routes = [
         component: MainComponent,
         canActivate: [AuthGuardService],
         children: [
-            { path: '', redirectTo: 'home', pathMatch: 'full' },
-            { path: 'home', loadChildren: loadHomeModule },
-            { path: 'customers', loadChildren: loadCustomersModule },
-            { path: 'employees', loadChildren: loadEmployeesModule }
+            {path: '', redirectTo: 'home', pathMatch: 'full'},
+            {path: 'home', loadChildren: loadHomeModule},
+            {path: 'customers', loadChildren: loadCustomersModule},
+            {path: 'employees', loadChildren: loadEmployeesModule},
+            {path: 'branches', loadChildren: loadBranchesModule}
         ]
     }
 ];
@@ -37,4 +43,5 @@ export const routes: Routes = [
     imports: [RouterModule.forChild(routes)],
     exports: [RouterModule]
 })
-export class MainRoutingModule { }
+export class MainRoutingModule {
+}
