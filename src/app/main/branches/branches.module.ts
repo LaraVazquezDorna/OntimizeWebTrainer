188aa71e0be252efdb18b6357e863154b7c2954d
Lara Vázquez Dorna <laritavdorna@gmail.com>
2020-06-11 12:33:06 +0200

    detalle sucursales
    Crear formulario de detalle

--- src/app/main/branches/branches.module.ts
+++ src/app/main/branches/branches.module.ts
@@ -4,6 +4,7 @@ import {CommonModule} from '@angular/common';
 import {BranchesRoutingModule} from './branches-routing.module';
 import {OntimizeWebModule} from "ontimize-web-ngx";
 import {BranchesHomeComponent} from "./branches-home/branches-home.component";
+import {BranchesDetailComponent} from "./branches-detail/branches-detail.component";
 
 @NgModule({
     imports: [
@@ -12,7 +13,8 @@ import {BranchesHomeComponent} from "./branches-home/branches-home.component";
         BranchesRoutingModule
     ],
     declarations: [
-        BranchesHomeComponent
+        BranchesHomeComponent,
+        BranchesDetailComponent
     ]
 })
 export class BranchesModule {
